eb649cfb84bcc2192edd7719131dfc3ea2df8042
jihan-nurlita <jihan.nurlita.id@gmail.com>
2026-09-17 16:58:29 +0700

    Fix splash screen mobile layout

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -211,7 +211,6 @@ class _SplashScreenState extends State<SplashScreen> {
                             'assets/splash.png',
                             width: double.infinity,
                             height: double.infinity,
-                            fit: BoxFit.contain,
                             alignment: Alignment.bottomCenter,
                           ),
                         ),
